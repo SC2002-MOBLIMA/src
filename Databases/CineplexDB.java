ceac093fbce23c4c5641bfa6e38b06d4b4cefdd2
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2022-10-31 16:24:08 +0800

    Update CineplexDB

--- Databases/CineplexDB.java
+++ Databases/CineplexDB.java
@@ -1,5 +1,6 @@
 package Databases;
 
+import java.lang.System;
 import Objects.Cineplex;
 import Objects.Cinema;
 
@@ -11,5 +12,9 @@ public class CineplexDB extends SerializeDB {
   public static String generateCinemaCode(Cineplex cnp, Cinema cnm) {
     String code = cnp.getCineplexName()[0] + cnp.getCineplexName()[1] + cnm.getCinemaNum().toString();
     return code;
+  }
+
+  public static int generateShowingId() {
+    return (int) (System.currentTimeMillis() / 1000);
   }
 }
