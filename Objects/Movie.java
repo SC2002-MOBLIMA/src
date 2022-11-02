68cec724aafc853d048ca7df05163c42b2f02b36
kaijun123 <kaijun123@gmail.com>
2022-11-03 02:13:04 +0800

    Added comparator functions for Movie

--- Objects/Movie.java
+++ Objects/Movie.java
@@ -1,133 +1,154 @@
 package Objects;
 
 import java.io.Serializable;
-import java.util.*;
-
-import Enums.MovieType;
+import java.util.ArrayList;
 import Enums.MovieStatus;
+import Enums.MovieType;
 
-public class Movie implements Serializable {
-    private String title;
-    private MovieStatus status;
-    private String synopsis;
-    private String director;
-    private ArrayList<String> cast;
-    private ArrayList<Review> reviewList = new ArrayList<Review>(); // Always initialized
-    private int saleCount;
-    private MovieType type;
-    private String endOfShowingDate;
+public class Movie implements Serializable, Comparable<Movie> {
+  private String title;
+  private MovieStatus status;
+  private String synopsis;
+  private String director;
+  private ArrayList<String> cast;
+  private ArrayList<Review> reviewList = new ArrayList<Review>(); // Always initialized
+  private int saleCount;
+  private MovieType type;
+  private String endOfShowingDate;
 
-    public Movie(String title, MovieStatus status, String synopsis, String director, ArrayList<String> cast,
-            int saleCount,
-            MovieType type, String endOfShowingDate) {
-        this.title = title;
-        this.status = status;
-        this.synopsis = synopsis;
-        this.director = director;
-        this.cast = cast;
-        this.saleCount = saleCount;
-        this.type = type;
-        this.endOfShowingDate = endOfShowingDate;
+  public Movie(String title, MovieStatus status, String synopsis, String director, ArrayList<String> cast,
+      int saleCount, MovieType type, String endOfShowingDate) {
+    this.title = title;
+    this.status = status;
+    this.synopsis = synopsis;
+    this.director = director;
+    this.cast = cast;
+    this.saleCount = saleCount;
+    this.type = type;
+    this.endOfShowingDate = endOfShowingDate;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public MovieStatus getStatus() {
+    return this.status;
+  }
+
+  public void setStatus(MovieStatus status) {
+    this.status = status;
+  }
+
+  public String getSynopsis() {
+    return this.synopsis;
+  }
+
+  public void setSynopsis(String synopsis) {
+    this.synopsis = synopsis;
+  }
+
+  public String getDirector() {
+    return this.director;
+  }
+
+  public void setDirector(String director) {
+    this.director = director;
+  }
+
+  public ArrayList<String> getCast() {
+    return this.cast;
+  }
+
+  public void setCast(ArrayList<String> cast) {
+    this.cast = cast;
+  }
+
+  public MovieType getType() {
+    return this.type;
+  }
+
+  public void setType(MovieType type) {
+    this.type = type;
+  }
+
+  public int getSalesCount() {
+    return this.saleCount;
+  }
+
+  public void incrementSaleCount() {
+    int count = getSalesCount();
+    this.saleCount = count++;
+  }
+
+  public void printMovieDetails() {
+    System.out.println("Title: " + title);
+    System.out.println("Status: " + status);
+    System.out.println("Synopsis: " + synopsis);
+    System.out.println("Director: " + director);
+    System.out.println("Cast: " + cast);
+    System.out.println("ReviewList: " + reviewList);
+    System.out.println("Type: " + type);
+    System.out.println("SaleCount: " + saleCount);
+    System.out.println("EndOfShowingDate: " + endOfShowingDate);
+  }
+
+  public void addReview(String name, int rating, String review) {
+    Review newReview = new Review(name, rating, review);
+    this.reviewList.add(newReview);
+  }
+
+  public double getOverallRating() {
+    int totalRating = 0;
+    int count = 0;
+    for (Review r : reviewList) {
+      totalRating += r.getRating();
+      count++;
+    }
+    int overallRating = totalRating / count;
+    return overallRating;
+  }
+
+  @Override
+  public int compareTo(Movie m) {
+    // compare by movie title
+    return this.title.compareToIgnoreCase(m.getTitle());
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    // compare by movie title
+    if (o == null) {
+      return false;
+    }
+    if (o.getClass() != this.getClass()) {
+      return false;
     }
 
-    public String getTitle() {
-        return this.title;
+    Movie m = (Movie) o;
+    if (this.title == m.getTitle()) {
+      return true;
     }
+    return false;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public MovieStatus getStatus() {
-        return this.status;
-    }
-
-    public void setStatus(MovieStatus status) {
-        this.status = status;
-    }
-
-    public String getSynopsis() {
-        return this.synopsis;
-    }
-
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
-    }
-
-    public String getDirector() {
-        return this.director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public ArrayList<String> getCast() {
-        return this.cast;
-    }
-
-    public void setCast(ArrayList<String> cast) {
-        this.cast = cast;
-    }
-
-    public MovieType getType() {
-        return this.type;
-    }
-
-    public void setType(MovieType type) {
-        this.type = type;
-    }
-
-    public int getSalesCount() {
-        return this.saleCount;
-    }
-
-    public void incrementSaleCount() {
-        int count = getSalesCount();
-        this.saleCount = count++;
-    }
-
-    public void printMovieDetails() {
-        System.out.println("Title: " + title);
-        System.out.println("Status: " + status);
-        System.out.println("Synopsis: " + synopsis);
-        System.out.println("Director: " + director);
-        System.out.println("Cast: " + cast);
-        System.out.println("ReviewList: " + reviewList);
-        System.out.println("Type: " + type);
-        System.out.println("SaleCount: " + saleCount);
-        System.out.println("EndOfShowingDate: " + endOfShowingDate);
-    }
-
-    public void addReview(String name, int rating, String review) {
-        Review newReview = new Review(name, rating, review);
-        this.reviewList.add(newReview);
-    }
-
-    public double getOverallRating() {
-        int totalRating = 0;
-        int count = 0;
-        for (Review r : reviewList) {
-            totalRating += r.getRating();
-            count++;
-        }
-        int overallRating = totalRating / count;
-        return overallRating;
-    }
-
-    // TODO:
-    // public int compareTo() {}
-    // public boolean equals(Object o) {}
-
-    // // Testing
-    // public static void main(String args[]) {
-    // String[] c = { "A", "Iron B" };
-    // Movie m = new Movie("Spider Man", MovieStatus.COMING_SOON, "Spider Man",
-    // "Spider Man", c, 0, MovieType.BLOCKBUSTER,
-    // "ABC");
-    // m.addReview("kaijun", 5, "sucks");
-    // m.addReview("kaijun", 1, "sucks");
-    // System.out.println(m.getOverallRating());
-    // System.out.println("works");
-    // }
+  // public static void main(String args[]) {
+  //   ArrayList<String> cast = new ArrayList<>();
+  //   cast.add("ABC");
+  //   cast.add("ABC");
+  //   String[] c = { "A", "Iron B" };
+  //   Movie m = new Movie("Spider Man", MovieStatus.COMING_SOON, "Spider Man",
+  //       "Spider Man", cast, 0, MovieType.BLOCKBUSTER, "ABC");
+  //   Movie m2 = new Movie("Spider Man", MovieStatus.COMING_SOON, "Spider Man",
+  //       "Spider Man", cast, 0, MovieType.BLOCKBUSTER, "ABC");
+  //   System.out.println(m2.equals(m));
+  //   m.addReview("kaijun", 5, "sucks");
+  //   m.addReview("kaijun", 1, "sucks");
+  //   System.out.println(m.getOverallRating());
+  //   System.out.println("works");
+  // }
 }
