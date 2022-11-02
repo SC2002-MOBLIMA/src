052d3a26bf629403a368d9fe0424da67fe1d341c
kaijun123 <kaijun123@gmail.com>
2022-11-03 03:35:03 +0800

    Added enum methods

--- Enums/DateType.java
+++ Enums/DateType.java
@@ -1,8 +1,27 @@
 package Enums;
 
 public enum DateType {
-  WEEKEND,
-  WEEKDAY,
-  PUBLIC_HOLIDAY
-  
+  WEEKEND("weekend"),
+  WEEKDAY("weekday"),
+  PUBLIC_HOLIDAY("public holiday");
+
+  private final String date;
+
+  private DateType(String date) {
+    this.date = date;
+  }
+
+  public String getDate() {
+    return this.date;
+  }
+
+  public static DateType fromString(String text) {
+    for (DateType d : DateType.values()) {
+      if (d.getDate().equalsIgnoreCase(text)) {
+        return d;
+      }
+    }
+    return null;
+  }
+
 }
--- Enums/MovieType.java
+++ Enums/MovieType.java
@@ -1,7 +1,28 @@
 package Enums;
 
+import Objects.Movie;
+
 public enum MovieType { // modified by aaron based on appendix A
-  REGULAR,
-  THREE_D,
-  BLOCKBUSTER
+  REGULAR("regular"),
+  THREE_D("3d"),
+  BLOCKBUSTER("blockbuster");
+
+  private String type;
+
+  private MovieType(String text) {
+    this.type = text;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public MovieType fromString(String text) {
+    for (MovieType type : MovieType.values()) {
+      if (type.getType().equalsIgnoreCase(text)) {
+        return type;
+      }
+    }
+    return null;
+  }
 }
