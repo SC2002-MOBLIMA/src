54fa26af7f8d3affdd8d9ce071aaa80293e33229
IvanLoke <102502850+IvanLoke@users.noreply.github.com>
2022-11-03 14:16:39 +0800

    Update MovieGoerModule.java

--- Modules/MovieGoerModule.java
+++ Modules/MovieGoerModule.java
@@ -2,6 +2,8 @@ package Modules;
 
 import java.util.Scanner;
 import java.io.*;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.attribute.FileStoreAttributeView;
 import java.util.*;
 import Databases.MovieDB;
 import Databases.MovieGoerDB;
@@ -11,6 +13,7 @@ import Objects.Cinema;
 import Objects.Showing;
 import Objects.MovieGoer;
 import Objects.Movie;
+import Objects.Review;
 import Enums.MovieStatus;
 import Comparators.SortByRating;
 import Comparators.SortBySales;
@@ -40,7 +43,7 @@ public class MovieGoerModule {
             System.out.print("Please enter your username: ");
             String username = sc.nextLine();
 
-            for (MovieGoer mg: readMovieGoers) {
+            for (MovieGoer mg : readMovieGoers) {
                 String mgUsername = mg.getName();
                 if (mgUsername.equals(username)) {
                     validUsername = true;
@@ -78,6 +81,8 @@ public class MovieGoerModule {
                     printMovieSearch(keywords, false, allmovies);
                     break;
                 case 3:
+                    System.out.print("Please enter the keywords: ");
+                    keywords = sc.nextLine();
                     printMovieSearch(keywords, true, allmovies);
                     break;
                 case 4:
@@ -101,22 +106,32 @@ public class MovieGoerModule {
     }
 
     public void printMovieSearch(String phrase, Boolean detailed, ArrayList<Movie> movies) {
+        System.out.println("***********************************************");
         for (Movie m : movies) {
             if (m.getStatus() == (MovieStatus.NOW_SHOWING) && m.getTitle().contains(phrase)) {
                 if (detailed == false) {
                     System.out.println(m.getTitle());
                 } else {
-                    System.out.println(m.getTitle());
-                    System.out.println(m.getStatus());
-                    System.out.println(m.getSynopsis());
-                    System.out.println(m.getDirector());
-                    System.out.println(m.getCast());
-                    System.out.println(m.getReviewList());
-                    System.out.println(m.getSalesCount());
-                    System.out.println(m.getType());
+                    System.out.println("Title: " + m.getTitle());
+                    System.out.println("Movie Status: " + m.getStatus());
+                    System.out.println("Movie Synopsis: " + m.getSynopsis());
+                    System.out.println("Movie Director: " + m.getDirector());
+                    System.out.print("Cast Members: ");
+                    ArrayList<String> castMembers = m.getCast();
+                    System.out.println(String.join(", ", castMembers));
+                    System.out.print("\nReviews: ");
+                    for (Review moviereview : m.getReviewList()) {
+                        System.out.println("Name: " + moviereview.getName());
+                        System.out.println("Rating: " + moviereview.getRating());
+                        System.out.println("Review: " + moviereview.getReview());
+                    }
+
+                    System.out.println("\nSales Count: " + m.getSalesCount());
+                    System.out.println("Movie Type: " + m.getType());
                 }
             }
         }
+        System.out.println("***********************************************");
     }
 
     public void printMovieByRating() {
