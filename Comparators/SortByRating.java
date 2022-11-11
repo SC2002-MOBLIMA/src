2edddebb935687eec96f1dad3c57e98f1b4d0bf4
Aaron Chua <81175540+aaronchualala@users.noreply.github.com>
2022-11-11 20:42:31 +0800

    Fix Top 5 Movie Sorting

--- Comparators/SortByRating.java
+++ Comparators/SortByRating.java
@@ -6,7 +6,7 @@ import Objects.Movie;
 
 public class SortByRating implements Comparator<Movie> {
     public int compare(Movie m0, Movie m1) {
-        if (m0.getOverallRating() > m1.getOverallRating()) {
+        if (m0.getOverallRating() < m1.getOverallRating()) {
             return 1;
         } else if (m0.getOverallRating() == m1.getOverallRating()) {
             return 0;
