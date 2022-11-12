efd8128cefe237cf9cab8013afeb23bf65ded1e6
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-11-12 23:32:02 +0800

    Removed Admin Flag

--- Modules/AdminModule.java
+++ Modules/AdminModule.java
@@ -38,7 +38,7 @@ public class AdminModule implements ModuleInterface, LoginInterface {
      */
     public AdminModule(Scanner sc) {
         this.sc = sc;
-        this.isLoggedIn = true; // TODO: toggle this later
+        this.isLoggedIn = false;
     }
 
     /**
