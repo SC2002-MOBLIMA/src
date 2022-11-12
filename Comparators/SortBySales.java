7ecf560f1976db78b6f545b730375a95f6d6ab46
IvanLoke <102502850+IvanLoke@users.noreply.github.com>
2022-11-12 22:48:54 +0800

    add javadoc for comparator

--- Comparators/SortBySales.java
+++ Comparators/SortBySales.java
@@ -4,7 +4,21 @@ import java.util.Comparator;
 
 import Objects.Movie;
 
+/**
+ * Represents the comparator class used for comparing two movies based on
+ * Movie sales.
+ * 
+ * @author Ivan Loke
+ * @version 1.0
+ * @since 2022-11-11
+ */
 public class SortBySales implements Comparator<Movie> {
+    /**
+     * Creates a new Comparator to compare two Movies based on Review Ratings
+     * 
+     * @param m0 First Movie object to be compared
+     * @param m1 Second Movie object to be compared
+     */
     public int compare(Movie m0, Movie m1) {
         if (m0.getSaleCount() < m1.getSaleCount()) {
             return 1;
